ce6f7e6eaf2add98ad8387fe4f6ae93a4e3d9606
unknown <zieglee1@gmatc.matc.edu>
2022-10-07 07:43:18 -0500

    Database seeding

---
+++ Persistence/Seed.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static void SeedData(DataContext context)
+        {
+            var Posts = new List<Post>
+            {
+                new Post{
+                    Title = "First Post",
+                    Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    Date = DateTime.Now.AddDays(-10)
+                },
+                new Post{
+                    Title = "Second Post",
+                    Body = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
+                    Date = DateTime.Now.AddDays(-5)
+                },
+                new Post{
+                    Title = "Third Post",
+                    Body = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+                    Date = DateTime.Now.AddDays(-1)
+                }
+            };
+
+            context.Posts.AddRange(Posts);
+            context.SaveChanges();
+        }
+    }
+}
