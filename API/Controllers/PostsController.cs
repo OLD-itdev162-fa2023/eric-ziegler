37368e2490af48e1cab1f5f3a9a32676c517a702
unknown <zieglee1@gmatc.matc.edu>
2022-10-10 09:25:10 -0500

    post creation endpoint

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -26,7 +26,7 @@ namespace API.Controllers
         }
 
         /// <summary>
-        /// GET api/post/[id]
+        /// GET api/posts/[id]
         /// </summary>
         /// <param name="id">Post id</param>
         /// <returns>A single post</returns>
@@ -40,6 +40,34 @@ namespace API.Controllers
             }
 
             return Ok(post);
+        }
+
+        /// <summary>
+        /// POST api/posts
+        /// </summary>
+        /// <param name="request">JSON request containing post fields</param>
+        /// <returns>A new post</returns>
+        /// <exception cref="Exception"></exception>
+        [HttpPost(Name = "Create")]        
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post
+            {
+                ID = request.ID,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if(success)
+            {
+                return Ok(post);
+            }
+
+            throw new Exception("Error creating post");
         }
     }
 }
